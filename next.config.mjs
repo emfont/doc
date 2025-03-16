bc5549a5fad7f8052c236f246519664320eff510
Elvis Mao <info@elvismao.com>
2025-03-16 09:57:28 +0800

    環境建設與目前的文檔

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @format */
+
+import { createMDX } from "fumadocs-mdx/next";
+
+const withMDX = createMDX();
+
+/** @type {import('next').NextConfig} */
+const config = {
+   // output: "export", // 如果要輸出靜態網頁會需要設定其他的畫面，可參考 https://fumadocs.vercel.app/docs/headless/search/orama#static-export
+    reactStrictMode: true
+};
+
+export default withMDX(config);
