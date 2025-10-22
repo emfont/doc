a2f25789d4a477f2f68aae0ade2c1c2360676ae0
Elvis Mao <info@elvismao.com>
2025-10-22 21:16:33 +0800

    feat: detailed mcp doc

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const withMDX = createMDX();
 const config = {
     output: "export", // 如果要輸出靜態網頁會需要設定其他的畫面，可參考 https://fumadocs.vercel.app/docs/headless/search/orama#static-export
     reactStrictMode: true,
+    images: { unoptimized: true },
 };
 
 export default withMDX(config);
